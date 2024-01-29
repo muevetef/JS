e68d13cefcfafff02b1e6b20821db74c228f6ca0
toni <easyrobotics2@gmail.com>
2024-01-29 12:10:18 +0100

    XMLRequest

--- 15-javascript-aincrono/js/script-ajax.js
+++ 15-javascript-aincrono/js/script-ajax.js
@@ -1,10 +1,21 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "./pelis.json");
+// xhr.open("GET", "./pelis.json");
+xhr.open("GET", "https://api.github.com/users/muevetef/repos");
 
 xhr.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200)
-    console.log(JSON.parse(this.responseText));
+  //Estados posibles de la peticion
+  /*
+  0: peticion no inicializada
+  1: conexión con el server establecida
+  2:petición recibida
+  3: procesando la peticion
+  4; peticion acabada y lista para usar
+  */
+  if (this.readyState === 4 && this.status === 200) {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+  }
 };
 
 xhr.send();
